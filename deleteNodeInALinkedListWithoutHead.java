c83d785ffe414cabb93f08f75585a70ba5b97a48
Shivansh Pandey <shivaay0898@gmail.com>
2023-11-06 22:09:14 +0530

    Added few new programs

---
+++ deleteNodeInALinkedListWithoutHead.java
@@ -0,0 +1,16 @@
+public class deleteNodeInALinkedListWithoutHead {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
---
+++ removeLinkedListElements.java
@@ -0,0 +1,34 @@
+public class removeLinkedListElements {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            while ((head != null) && (head.val == val)) {
+                head = head.next;
+            }
+            ListNode temp = head;
+            while ((temp != null) && (temp.next != null)) {
+                while ((temp.next != null) && (temp.next.val == val)) {
+                    temp.next = temp.next.next;
+                }
+                temp = temp.next;
+            }
+            return head;
+        }
+    }
+}
